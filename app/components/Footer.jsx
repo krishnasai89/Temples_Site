3688d50c19a8dc1004b8c7ffda12d59d1670f272
Krishnasai_89 <krishnasaivellampalli@gmail.com>
2026-05-06 21:36:23 +0530

    update

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -23,6 +23,38 @@ const Footer = () => {
             </p>
             {/* Social Icons */}
             <div className="flex space-x-4">
+              {/* GitHub Icon */}
+              <a
+                href="https://github.com/krishnasai89"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-900 border border-gray-800 text-gray-400 hover:bg-orange-500/10 hover:text-orange-500 hover:border-orange-500/50 transition-all duration-300"
+              >
+                <svg
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-3.975-1.395-.09-.225-.48-1.395-.825-1.68-.285-.15-.69-.435-.015-.435.63-.015 1.08.585 1.23.825.72 1.215 1.875.87 2.325.66.075-.525.285-.87.51-1.065-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.3.255.54.735.54 1.485 0 1.065-.015 1.92-.015 2.19 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" />
+                </svg>
+              </a>
+
+              {/* LinkedIn Icon */}
+              <a
+                href="https://www.linkedin.com/in/krishna-sai-vellampalli/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-900 border border-gray-800 text-gray-400 hover:bg-orange-500/10 hover:text-orange-500 hover:border-orange-500/50 transition-all duration-300"
+              >
+                <svg
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                </svg>
+              </a>
+
               {/* YouTube Icon */}
               <a
                 href="#"
@@ -36,9 +68,10 @@ const Footer = () => {
                   <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                 </svg>
               </a>
+
               {/* Instagram Icon */}
               <a
-                href="#"
+                href="https://www.instagram.com/swananda2026/"
                 className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-900 border border-gray-800 text-gray-400 hover:bg-orange-500/10 hover:text-orange-500 hover:border-orange-500/50 transition-all duration-300"
               >
                 <svg
@@ -49,6 +82,7 @@ const Footer = () => {
                   <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" />
                 </svg>
               </a>
+
               {/* X / Twitter Icon */}
               <a
                 href="#"
@@ -72,17 +106,17 @@ const Footer = () => {
             </h3>
             <ul className="space-y-3">
               {[
-                "Home",
-                "Temple Stories",
-                "Architectural Wonders",
-                "Deities & Legends",
-              ].map((item, index) => (
+                { name: "Home", path: "/" },
+                { name: "Stories", path: "/Stories" },
+                { name: "Dances", path: "/Dances" },
+                { name: "About", path: "/About" },
+              ].map((name, path, index) => (
                 <li key={index}>
                   <Link
-                    href="#"
+                    href="path"
                     className="text-gray-400 hover:text-orange-400 text-sm transition-colors duration-300"
                   >
-                    {item}
+                    {name}
                   </Link>
                 </li>
               ))}
